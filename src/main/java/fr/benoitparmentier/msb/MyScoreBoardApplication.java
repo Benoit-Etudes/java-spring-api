52802c5eee41ca6b3474e03b2e682c9a8d8e13d1
BenoitPrmt <benoitparmentier45@gmail.com>
2024-12-19 09:36:00 +0100

    chore: First commit

---
+++ src/main/java/fr/benoitparmentier/msb/MyScoreBoardApplication.java
@@ -0,0 +1,13 @@
+package fr.benoitparmentier.msb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyScoreBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyScoreBoardApplication.class, args);
+	}
+
+}
